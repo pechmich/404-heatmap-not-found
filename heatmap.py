eacdaff406f0a491a1332f51fe8a63863fa1b200
Michal Pech <pechmic3@fit.cvut.cz>
2023-04-12 12:32:42 +0200

    export heatmap without legend

--- heatmap.py
+++ heatmap.py
@@ -2,11 +2,14 @@ import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 
-# Generate an array of 100 random rectangles
-rectangles = np.random.randint(0, 300, size=(100, 4))
+filename = 'rectangles_3_1k.txt'
 
-# Ensure that the left-bottom points are less than the right-upper points
-rectangles[:, 2:] = np.maximum(rectangles[:, :2], rectangles[:, 2:])
+# read rectangles from input file
+rectangles = []
+with open(filename, 'r') as f:
+    for line in f:
+        rect = tuple(map(int, line.strip().split(',')))
+        rectangles.append(rect)
 
 # Create an empty image with the same dimensions as the heatmap
 heatmap = np.zeros((300, 300), dtype=np.float32)
@@ -16,20 +19,11 @@ for rect in rectangles:
     # Add 1 to each pixel in the rectangle
     heatmap[y1:y2, x1:x2] += 1
 
-# Create a heatmap plot of the data
-fig, ax = plt.subplots()
-im = ax.imshow(heatmap, cmap='gray')
+# for every pixel in the heatmap, decrease its value to 255 if it is greater than 255
+heatmap[heatmap > 255] = 255
 
-# Add a colorbar to show the mapping of values to grayscale colors
-cbar = ax.figure.colorbar(im, ax=ax)
+# Scale the array to the range [0, 255]
+arr = heatmap.astype(np.uint8)
 
-# Set the title and axis labels
-ax.set_title("Grayscale Heatmap")
-ax.set_xlabel("X-axis")
-ax.set_ylabel("Y-axis")
-
-# Display the plot
-plt.show()
-
-# Save the plot to a file
-fig.savefig("heatmap.png")
+# Save the array as an image in shades of gray
+cv2.imwrite("heatmap.png", arr)
