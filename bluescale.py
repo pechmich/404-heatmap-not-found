49222b0d24de735db79a3ea93ab7232be6a7e191
Michal Pech <pechmic3@fit.cvut.cz>
2023-04-12 14:05:46 +0200

    convert heatmap to bluescale

---
+++ bluescale.py
@@ -0,0 +1,22 @@
+from PIL import Image
+
+# Open the image and convert it to RGB mode
+img = Image.open('heatmap.png').convert('RGB')
+
+# Create a new empty image of the same size
+blue_img = Image.new('RGB', img.size)
+
+# Loop through each pixel in the image and set the blue channel value
+for x in range(img.width):
+    for y in range(img.height):
+        # get the pixel value
+        _, _, b = img.getpixel((x, y))
+
+        # Set the blue channel value to the original grayscale value
+        blue_img.putpixel((x, y), (0, 0, b))
+
+        # Alternatively, you can set the blue channel value to a fixed value, e.g. 128
+        # blue_img.putpixel((x, y), (0, 0, 128))
+
+# Save the new image as a PNG file
+blue_img.save('bluescale.png')
